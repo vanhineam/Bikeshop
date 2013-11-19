aa4293cf4eebf6ec9d557481ae1ee64ab0dd7212
Adam Van Hine <vanhineam@appstate.edu>
2013-11-18 22:12:39 -0500

    destroy works

--- destroy.sql
+++ destroy.sql
@@ -6,20 +6,15 @@
 -- Define space for tables
 SET @tables = NULL;
 
--- Set database name here
-SET @database = 'bikeshop'
-
 -- Get list of all tables in database
 SELECT GROUP_CONCAT(table_schema, '.', table_name) INTO @tables
 FROM information_schema.tables
-WHERE table_schema = @database;
+WHERE table_schema = 'bikeshop'; # database name here
 
 -- Format all the "Drop" commands
-SET @tables = CONCAT('DROP TABLES', @tables);
+SET @tables = CONCAT('DROP TABLE ', @tables);
 
 -- Prepare and execute commands
 PREPARE stmt FROM @tables;
 EXECUTE stmt;
-
--- Free up some space
 DEALLOCATE PREPARE stmt;
